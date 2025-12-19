8e87ffc2c0d3d70bcb5d5c3a5f213147ed9da650
Abhijith <abhijithpmenon2004@gmail.com>
2025-12-19 09:39:43 +0530

    Delete dmenu directory

--- dmenu/themes/material.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-    [SchemeNorm]      = { "#D8DEE9", "#0f111a" },
-    [SchemeSel]       = { "#3B4252", "#FF4151" },
-    [SchemeOut]       = { "#D8DEE9", "#81A1C1" },
-};
-
