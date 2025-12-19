a5a99113e5cbdfc8589400430fe81856db423101
Abhijith <abhijithpmenon2004@gmail.com>
2025-12-20 01:31:46 +0530

    Add files via upload

---
+++ dmenu/config.h
@@ -0,0 +1,42 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 600;                    /* minimum width when centered */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
+static const unsigned int alpha = 0xcc;     /* Amount of opacity. 0xff is opaque             */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const int user_bh = 0;               /* add an defined amount of pixels to the bar height */
+
+static const char *fonts[] = {
+	"JetBrainsMono Nerd Font Mono:style:bold:size=11"
+};
+
+#include "themes/decayce.h"
+
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+// static const char *colors[SchemeLast][2] = {
+// 	/*     fg         bg       */
+// 	[SchemeNorm] = { "#cbced3", "#0d0f18" },
+// 	[SchemeSel] = { "#262831", "#90ceaa" },
+// 	[SchemeOut] = { "#000000", "#00ffff" },
+// };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines    = 10;
+static unsigned int columns    = 2;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
---
+++ dmenu/themes/rose-pine.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#e0def4", "#191724" },
+	[SchemeSel] = { "#403d52", "#9ccfd8" },
+	[SchemeOut] = { "#000000", "#56949f" },
+};
